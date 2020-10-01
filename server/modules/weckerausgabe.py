9d6b8f0d669c8c94c720cdf7df259113f7f74199
JakobPrie <60171863+JakobPrie@users.noreply.github.com>
2020-10-01 22:35:31 +0200

    Update weckerausgabe.py

--- server/modules/weckerausgabe.py
+++ server/modules/weckerausgabe.py
@@ -4,33 +4,13 @@ SECURE = True # Damit es von fortlaufenden module naufgerufen werden kann
 
 def handle(text, luna, profile):
     user = text.get('Benutzer')
-    text = text.get('Text')
-    path = "/home/pi/Desktop/LUNA/server/modules/resources/aufstehen.wav"
+    to_say = text.get('Text')
+    ton = text.get('Ton')
+    path = luna.path + "/modules/resources/Weckertöne/" + ton
     
-    luna.say(text, user=user)
-    play(path, luna)
-    
-def play(pfad, luna):
-    try:
-        chunk = 1024
-        
-        format = {'format': 8,
-                  'channels':1,
-                  'rate':88200,
-                  'chunk':chunk}
-        wav = wave.open(pfad, 'rb')
-        wav_data = wav.readframes(chunk)
-        audio_buffer = []
-        while wav_data:
-            audio_buffer.append(wav_data)
-            wav_data = wav.readframes(chunk)
-        audio_buffer.append('Endederdurchsage')
-        luna.audio_Output.playback_audio_format = format
-        luna.audio_Output.play(audio_buffer)
-        
-    except Exception as e:
-        print(f"Abbruch durch Fehler: {e}")
-        
+    if luna.local_storage['module_storage']['phillips_hue']['Bridge-IP'] != '':
+        luna.start_module(user=text['Benutzer'], name='phillips_hue', text='Mach das Licht weiß')
+    luna.play(path=path)
 
 def isValid(text):
     return False
