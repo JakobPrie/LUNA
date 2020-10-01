596f8c351e10f1453ffe91463d0c6c5805dc8338
JakobPrie <60171863+JakobPrie@users.noreply.github.com>
2020-10-01 22:17:33 +0200

    Update Ausgabe.py

--- server/modules/Ausgabe.py
+++ server/modules/Ausgabe.py
@@ -1,13 +1,7 @@
-PRIORITY = 9
 SECURE = True
 
-def isValid (text):
-	text = text.lower()
-	if 'abbruch' in text:
-		return True
-	elif 'abbrechen' in text:
-		return True
-	
-def handle (text, luna, profile):
-	print('[INFO] Befehl abgebrochen!')
-	#luna.say('Befehl abgebrochen')
+def isValid(text):
+    return False
+
+def handle(text, luna, profile):
+    luna.say(text)
