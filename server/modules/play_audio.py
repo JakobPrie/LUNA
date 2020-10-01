dfcd7d84abd2262e16a46a49eff6e8f7e61e9336
JakobPrie <60171863+JakobPrie@users.noreply.github.com>
2020-10-01 22:28:11 +0200

    Delete play_audio.py

--- server/modules/play_audio.py
+++
@@ -1,9 +0,0 @@
-
-def isValid(text):
-	text = text.lower()
-	if 'spiel' in text and 'ab' in text:
-		return True
-	
-def handle(text, luna, profile):
-	luna.play(path="/home/pi/Desktop/LUNA/server/modules/resources/aufstehen.wav", room='Schlafzimmer')
-	print("Modul abgeschlossen...")
