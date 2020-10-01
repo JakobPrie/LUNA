ca6767d93a0099a460134006b95fbed6db0a3ae1
JakobPrie <60171863+JakobPrie@users.noreply.github.com>
2020-10-01 22:18:24 +0200

    Update backup.py

--- server/modules/backup.py
+++ server/modules/backup.py
@@ -6,6 +6,8 @@ def isValid(text):
     text = text.lower()
     if ('erstell' in text or 'mach' in text or 'sicher' in text) and ('backup' in text or 'speicher' in text or 'date' in text):
         return True
+    else:
+        return False
 
 def check(thing):
     if type(thing) == type({'test':'test'}):
@@ -60,3 +62,38 @@ def handle(text, luna, profile):
     with open(luna.path + '/LUNA_LOG.json','w') as json_file:
         json.dump(backup_json, json_file, indent=4, ensure_ascii=False)
     luna.say('Die Daten wurden gespeichert.')
+
+def batchGen(batch):
+    """
+    With the batchGen-function you can generate fuzzed compare-strings
+    with the help of a easy syntax:
+        "Wann [fährt|kommt] [der|die|das] nächst[e,er,es] [Bahn|Zug]"
+    is compiled to a list of sentences, each of them combining the words
+    in the brackets in all different combinations.
+    This list can then fox example be used by the batchMatch-function to
+    detect special sentences.
+    """
+    outlist = []
+    ct = 0
+    while len(batch) > 0:
+        piece = batch.pop()
+        if "[" not in piece and "]" not in piece:
+            outlist.append(piece)
+        else:
+            frontpiece = piece.split("]")[0]
+            inpiece = frontpiece.split("[")[1]
+            inoptns = inpiece.split("|")
+            for optn in inoptns:
+                rebuild = frontpiece.split("[")[0] + optn
+                rebuild += "]".join(piece.split("]")[1:])
+                batch.append(rebuild)
+    return outlist
+
+def batchMatch(batch, match):
+    t = False
+    if isinstance(batch, str):
+        batch = [batch]
+    for piece in batchGen(batch):
+        if piece.lower() in match.lower():
+            t = True
+    return t
